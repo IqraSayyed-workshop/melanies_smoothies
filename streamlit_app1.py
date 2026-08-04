a63eaa7e03e169a0f78335d293bb27e1a293a9e4
IqraSayyed-workshop <iqrasayyed123456@gmail.com>
2026-08-04 17:51:07 +0530

    Refactor session handling and clean imports
    Removed unused imports and updated session handling.

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,7 +1,6 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col, when_matched
+from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title(":cup_with_straw: Pending Smoothies order! :cup_with_straw:")
@@ -9,8 +8,8 @@ st.write(
   """Orders that need to be filled.
   """
 )
-
-session = get_active_session()  
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 my_dataframe =session.table("smoothies.public.orders").filter(col("ORDER_FILLED") == 0).collect()
 editable_df = st.data_editor(my_dataframe)
