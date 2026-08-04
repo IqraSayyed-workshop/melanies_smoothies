0667623a6e0944f6c184f10a995665a4f1b7e539
IqraSayyed-workshop <iqrasayyed123456@gmail.com>
2026-08-04 17:26:22 +0530

    Fix API URL formatting in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if ingredients_list:
 
     # Call SmoothieFroot API
     smoothiefroot_response = requests.get(
-        f"https://my.smoothiefroot.com/api/fruit/{search_on}"
+        "https://my.smoothiefroot.com/api/fruit/{search_on}"
     )
 
    # st.write(smoothiefroot_response.json())
